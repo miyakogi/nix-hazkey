1e7a2eeb9c8bb85f940b32a7704600d1b795752d
miyaco <miyako.dev@gmail.com>
2026-02-11 17:46:30 +0900

    bump to hazkey 0.2.1

--- internal/prebuilt/fcitx5-hazkey.nix
+++ internal/prebuilt/fcitx5-hazkey.nix
@@ -1,10 +1,10 @@
 {fetchzip}: let
-  version = "0.2.0";
+  version = "0.2.1";
 in
   fetchzip {
     name = "fcitx5-hazkey-bin";
     inherit version;
     url = "https://github.com/7ka-Hiira/fcitx5-hazkey/releases/download/${version}/fcitx5-hazkey-${version}-x86_64.tar.gz";
-    hash = "sha256-agpqU8uVpmGJEnqQPsZBv3uSOw9pD0iri3/R/hRAACA=";
+    hash = "sha256-jwv1UTRz/FVHmeaumwP45Q4JZcSuZHTrF2/PAzrxeC8=";
     stripRoot = false;
   }
